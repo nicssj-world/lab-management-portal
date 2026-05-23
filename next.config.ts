109a09149136eb57f3a92c5b7675ab0eff3e6ed0
Nics SJ <nics.siriwatj@gmail.com>
2026-05-23 23:14:25 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pdf-parse', 'canvas'],
+  serverExternalPackages: ['canvas'],
   devIndicators: {
     position: 'bottom-right',
   },
